f0961842dbc1fe294537233372e6ad58d6a15f06
vajdzius <vajdzius@gmail.com>
2017-11-13 21:35:30 +0200

    Created table on php code
    Created files: bakery-data.php
    data-table.php

---
+++ app/app.php
@@ -0,0 +1,5 @@
+<?php
+
+$data = @include_once('model/bakery-data.php');
+
+@include_once('view/data-table.php');
---
+++ app/view/data-table.php
@@ -0,0 +1,54 @@
+<?php
+
+$days = $keys = '';
+
+	foreach ($data as $key => $value) {
+					$days .= "<th class=\"table-th\" colspan=\"5\">$key</th>";
+					$keys .= "<th>VL</th><th>PG</th><th>PR</th><th>SG</th><th>GL</th>";
+					//echo '<th class="table-th" colspan="5">'.$key.'</th>';
+
+			foreach ($value as $name = >$data){
+				// name        //data
+				"Varškės" => [7, 10, 8, 2, 7],
+
+				foreach ($data as $amount) {
+					//amount
+					3
+			}
+		}
+	}
+?>
+
+<table>
+	<thead>
+		<tr>
+			<th class="table-th" rowspan="2">Pavadinimas</th>
+			<?php
+				 echo $days;
+			?>
+		</tr>
+		<tr>
+			 <?php
+				 echo $keys;
+			?>	
+		</tr>
+
+	</thead>
+</table>
+
+<?php
+
+var_dump($data);
+return;
+
+$html = '';
+
+$html .= '<table>';
+$html .= '<thead>';
+
+$html .= '</thead>';
+$html .= '</table>';
+
+echo $html;
+
+return;
