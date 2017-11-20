e162f3128e7d3e7061df0ddfef8a2ea84cfdef07
vajdzius <vajdzius@gmail.com>
2017-11-20 20:55:59 +0200

    Added functions updateDate or createDate, createNewProduct
    Added functions updateDate or createDate, createNewProduct for HTML form

--- create.php
+++ create.php
@@ -24,12 +24,41 @@ function objectToArray(stdClass $obj) : array
 
 function updateDate (&$existing_data, $new_data)
 {
+	print_r($existing_data);
+	print_r($new_data);
+
 	if (isset($existing_data[$new_data ['date']]))
 	{
 		echo 'YES';
+
+			if (isset($existing_data[$new_data ['date']][$new_data['product']]))
+			echo "ERROR";
+
+			else
+			{
+				$existing_data = createNewProduct($existing_data, $new_data);
+			}
+		
 	}
 	else 
 	{
-		echo 'NO';
+
+		$existing_data[$new_data['date']] = [];
+		$existing_data = createNewProduct($existing_data, $new_data);
+
+		
 	}
+	print_r($existing_data);
+}
+
+function createNewProduct ($existing_data, $new_data)
+{
+	$existing_data[$new_data['date']][$new_data['product']] = [
+			$new_data["VL"],
+			$new_data["PG"],
+			$new_data["PR"],
+			$new_data["SG"],
+			$new_data["GL"]
+		];
+	return $existing_data;
 }
